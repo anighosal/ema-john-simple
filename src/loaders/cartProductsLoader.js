7e769a98701a4662e51c6a132c4aa39e6ed45d3f
anighosal <121676026+anighosal@users.noreply.github.com>
2023-04-05 22:04:50 +0600

    removecart added

---
+++ src/loaders/cartProductsLoader.js
@@ -0,0 +1,24 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
+const cartProductsLoader = async () => {
+  const loadedProducts = await fetch("products.json");
+  const products = await loadedProducts.json();
+
+  const storedCart = getShoppingCart();
+  const savedCart = [];
+
+  for (const id in storedCart) {
+    const addedProduct = products.find((product) => product.id === id);
+    if (addedProduct) {
+      const quantity = storedCart[id];
+      addedProduct.quantity = quantity;
+      savedCart.push(addedProduct);
+    }
+  }
+
+  //   if u need to send two things
+  //   return [products, savedCart];
+  return savedCart;
+};
+
+export default cartProductsLoader;
